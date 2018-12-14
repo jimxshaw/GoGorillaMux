fea3776f485e62645892d77733373f6c38831f72
Jim Shaw <jimxshaw@gmail.com>
2018-12-14 16:59:35 -0600

    Add methods to main_test.go file.

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"log"
 	"os"
 	"testing"
 
@@ -11,7 +12,8 @@ var a main.App
 
 func TestMain(m *testing.M) {
 	a = main.App{}
-	a.Initialize(os.Getenv("TEST_DB_USERNAME"),
+	a.Initialize(
+		os.Getenv("TEST_DB_USERNAME"),
 		os.Getenv("TEST_DB_PASSWORD"),
 		os.Getenv("TEST_DB_NAME"))
 
@@ -23,3 +25,22 @@ func TestMain(m *testing.M) {
 
 	os.Exit(code)
 }
+
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+	id SERIAL,
+	name TEXT NOT NULL,
+	price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+	CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
